3f3c40b1d9f4c8cea1cb1505eb7d9c8afd22ded6
wsaidac <48682066+wsaidac@users.noreply.github.com>
2019-10-02 12:41:42 +0200

    added bc channel identifier

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,20 @@ const Home = () => (
     <Head>
       <title>Home</title>
       <link rel='icon' href='/static/favicon.ico' importance='low' />
+  <script>
+ bcChannelIdentifier = “dev.rapreactpoc.com:4000”;
+</script>
+  
+<script type=“text/javascript”>
+(function() {
+  this.bcHostname = ‘cg.sb.blueconic.net’;
+  var protocol = ((“https:” == document.location.protocol) ? “https://” : “http://“);
+  var bcScript = document.createElement(‘script’);
+  bcScript.async = true;
+  bcScript.src = protocol + this.bcHostname + ‘/frontend/static/javascript/blueconic/blueconic.min.js’;
+  (document.getElementsByTagName(‘head’)[0] || document.getElementsByTagName(‘body’)[0]).appendChild(bcScript);
+})();
+</script>
     </Head>
 
     <div className='hero'>
